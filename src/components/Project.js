f299da0cd1089d506571d04f68a89f27ea753918
github username <sharnellgrant@gmail.com>
2022-04-24 23:05:45 -0400

    Added the project component and schema

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import sanityClient from "../client.js"
 
 export default function Project() {
-  return <h1>Project Page!</h1>
+
+  const [projectData, setProjectData] = useState;
+
+  useEffect(() => {
+     sanityClient.fetch(`*[_type == "project"]{
+       title, 
+       date,
+       place,
+       description,
+       projectType,
+       link,
+       tags
+     }`).then((data) => setProjectData(data))
+     .catch(console.error)
+  }, []);
+
+  return (
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive"> My Projects</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12"> Welcome to my projects page!</h2>
+        <section className="grid grid-cols-2 gap-8">
+          <article className="relative rounded-lg shadow-xl bg-white p-16">
+            <h3></h3>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+              <p></p>
+              <span></span>
+            </div>
+          </article>
+        </section>
+      </section>
+    </main>
+  )
+  
 }
